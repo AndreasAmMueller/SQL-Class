a2c72dd5aeaf174330c103aaba839b643d973093
Andreas Mueller <webmaster@am-wd.de>
2014-11-15 00:44:31 +0100

    Small Update
    Update on Readme:
    - Added code example
    
    Update on Class:
    - comments (a few)
    - Exception on cli w/o HTML Tags
    - Bugfix on fetch_object for SQLite
    
    Signed-off-by: Andreas Mueller <webmaster@am-wd.de>

--- README.md
+++ README.md
@@ -8,4 +8,42 @@ So you can use this class programming the App and while shipping you can set the
 __Requirement:__ correct SQL Syntax and no slangs!
 
 Functions for creating and reading a Dump included.
-__CAUTION:__ SQLite and MySQL Dumps are different! Also be careful with commands in your Dump file. Multiline comments with /* */ currently not supported.
+
+__CAUTION:__ SQLite and MySQL Dumps are different! Also be careful with commands in your Dump file. Multiline comments with /\* */ currently not supported.
+
+```
+<?php
+// load the Class
+require_once __DIR__."/sql.class.php";
+
+// Create my Object, here I want a MySQL Connection
+// We will use our static Function to create one
+// User:     TestUser
+// Password: fromErlangenToMunich
+// Database: exampleBase
+// Port:     3307
+// Host:     123.321.123.321
+$sql = SQL::MySQL("TestUser", "fromErlangenToMunich", "exampleBase", 3307, "123.321.123.321");
+
+// open the connection
+$sql->open();
+
+// Example Query
+// I just want to get all information from all users with firstname George
+$res = $sql->query("SELECT * FROM users WHERE firstname = 'George'");
+
+// tell me the number of results (lines)
+$count = $sql->num_rows($res);
+
+// print out all results
+echo "We have $count Results to Present".PHP_EOL;
+echo "---------------------------------".PHP_EOL;
+while ($row = $sql->fetch_object($res)) {
+	echo $row->firstname . " " . $row->lastname.PHP_EOL;
+}
+
+// close connection
+$sql->close();
+
+?>
+```
--- sql.class.php
+++ sql.class.php
@@ -4,12 +4,12 @@
  * sql.class.php
  *
  * @author Andreas Mueller <webmaster@am-wd.de>
- * @version 1.0-20141112
+ * @version 1.0-20141115
  *
  * @description
  * This class tries to provide (full) support for
  * - MySQL
- * - SQLite
+ * - SQLite3
  * using only one function-name
  *
  * Correct syntax (no slangs) required!
@@ -32,8 +32,13 @@ class SQL {
 
 	// static function to create mysql-instance
 	public static function MySQL($user, $password, $database, $port = 3306, $host = '127.0.0.1') {
-		if (!class_exists('mysqli'))
+		if (!class_exists('mysqli')) {
+			if (php_sapi_name() == 'cli') {
+				throw new Exception('Error: MySQLi Class not installed! Details at http://php.net/manual/de/book.mysqli.php');
+			} else {
 				throw new Exception('<b>Error:</b> MySQLi Class not installed! Details <a href="http://php.net/manual/de/book.mysqli.php">here</a>');
+			}
+		}
 
 		$self = new self('mysql');
 		$self->setHost($host);
@@ -48,8 +53,13 @@ class SQL {
 
 	// static function to create sqlite-instance
 	public static function SQLite($file, $password = '') {
-		if (!class_exists('SQLite3'))
+		if (!class_exists('SQLite3')) {
+			if (php_sapi_name() == 'cli') {
+				throw new Exception('Error: SQLite3 Class not installed! Details at http://php.net/manual/de/book.sqlite3.php');
+			} else {
 				throw new Exception('<b>Error:</b> SQLite3 Class not installed! Details <a href="http://php.net/manual/de/book.sqlite3.php">here</a>');
+			}
+		}
 
 		$self = new self('sqlite');
 		$self->setFile($file);
@@ -121,11 +131,12 @@ class SQL {
 	public function open() {
 		return ($this->type == 'sqlite') ? $this->openSQLite() : $this->openMySQL();
 	}
-
+	// wide used alias
 	public function connect() {
 		return $this->open();
 	}
 
+	// private helper to manage errors in SQLite
 	private function openSQLite() {
 		$switch = SQLITE3_OPEN_READWRITE | SQLITE3_OPEN_CREATE;
 		$c = new SQLite3($this->file, $switch, $this->password);
@@ -137,6 +148,7 @@ class SQL {
 		return true;
 	}
 
+	// private helper to manage conn errors in MySQL and encoding of transfer
 	private function openMySQL() {
 		$c = new mysqli($this->host, $this->user, $this->password, $this->database, $this->port);
 		if ($c->connect_errno) {
@@ -165,7 +177,7 @@ class SQL {
 		}
 		return true;
 	}
-
+	// commonly used alias
 	public function disconnect() {
 		return $this->close();
 	}
@@ -176,21 +188,27 @@ class SQL {
 		return $this->con->query($query);
 	}
 
+	// returns error message of the last executed query
 	public function error() {
 		return ($this->type == 'sqlite') ? $this->con->lastErrorMsg() : $this->con->error;
 	}
 
 	/* --- Results ---
-	--------------- */
+		--------------- */
+	// fetch queried results as array
+	// this will the wohle array at once
 	public function fetch_array($result) {
 		return ($this->type == 'sqlite') ? $result->fetchArray() : $result->fetch_array();
 	}
 
+	// fetch queried results as objects
+	// this will return each time a new object, or NULL if there are no more results
 	public function fetch_object($result) {
 		if ($this->type == 'sqlite') {
+			// SQLite we need to built ourselves
 			$array = $result->fetchArray();
 			if (empty($array))
-					return;
+					return NULL;
 
 			$res = new stdClass();
 			foreach ($res as $key => $val)
@@ -202,6 +220,7 @@ class SQL {
 		}
 	}
 
+	// tell me the number of results
 	public function num_rows($result) {
 		if ($this->type == 'sqlite') {
 			$count = 0;
@@ -214,10 +233,12 @@ class SQL {
 		}
 	}
 
+	// tell me the id (value from the field with AUTO_INCREMENT which is commonly the unique primary key)
 	public function insert_id() {
 		return ($this->type == 'sqlite') ? $this->con->lastInsertRowID() : $this->con->insert_id;
 	}
 
+	// tell the number of rows affected by the last query
 	public function affected_rows() {
 		return ($this->type == 'sqlite') ? $this->con->changes() : $this->con->affected_rows;
 	}
@@ -247,6 +268,7 @@ class SQL {
 		return $tables;
 	}
 
+	// get me the structure of $table
 	private function getStructure($table) {
 		$file = array();
 		$file[] = '';
@@ -269,6 +291,7 @@ class SQL {
 		return implode(PHP_EOL, $file);
 	}
 
+	// get me the data of $table
 	private function getData($table) {
 		$file = array();
 		$file[] = '';
@@ -299,6 +322,12 @@ class SQL {
 		return implode(PHP_EOL, $file);
 	}
 
+	// create a SQL Dump for Backup (SQLite and MySQL are not compatible!!!)
+	// first tests for a converter in progress
+	// $part can be either 'structure', 'data' or both seperated by comma
+	// $tables can be a comma seperated list or empty for all tables
+	// return value is a valid dumpfile (loooong string)
+	// just write it down to a file!
 	public function getDump($part = 'structure,data', $tables = '') {
 		$todo = explode(',', $part);
 
@@ -322,7 +351,7 @@ class SQL {
 		$file[] = "--";
 		$file[] = "-- Time: ".date('d. F Y H:i');
 		$file[] = '';
-		$file[] = 'SET FOREIGN_KEY_CHECKS = 0;';
+		$file[] = 'SET FOREIGN_KEY_CHECKS = 0;'; // important for realtions on ImmoDB
 
 		if (empty($tables)) {
 			$tbls = $this->getTables();
@@ -340,10 +369,14 @@ class SQL {
 
 		$this->close();
 
-		$file[] = 'SET FOREIGN_KEY_CHECKS = 1;';
+		$file[] = 'SET FOREIGN_KEY_CHECKS = 1;'; // important for relations on ImmoDB
 		return implode(PHP_EOL, $file);
 	}
 
+	// reads a Dumpfile and executes it
+	// if $returnError is TRUE the error message will be returned
+	// else return value is TRUE or FALSE to show success
+	// $dump can be a dump file read into a string or already split into an array (each line an enty)
 	public function restoreDump($dump, $returnError = false) {
 		$file = (is_array($dump)) ? $dump : preg_split("/(\r\n|\n|\r)/", $dump);
 		$line = 0; $lines = count($file);
@@ -413,7 +446,6 @@ class SQL {
 		} else if ($returnError) {
 				return $error;
 		} else {
-			//die(nl2br($error));
 			return false;
 		}
 	}
